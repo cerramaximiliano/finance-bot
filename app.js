7b4b6afe3f22574deabd0837df65ae218f8f93b4
Maximiliano Cerra <cerramaximiliano@gmail.com>
2024-08-14 10:56:15 -0300

    fix/ app logger

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ bot.on("polling_error", console.log);
 mongoose
   .connect(URL_DB)
   .then(() => logger.info("Conectado a MongoDB"))
-  .catch((err) => console.error("Error al conectar a MongoDB", err));
+  .catch((err) => logger.error("Error al conectar a MongoDB", err));
 
 require("./server/tasks/cronJobs");
 const logger = require("./server/utils/logger");
